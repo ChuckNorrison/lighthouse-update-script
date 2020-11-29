8e2d1cf8fc908287bc59b80a05c7fece070b322f
Chuck Norrison <2964146+ChuckNorrison@users.noreply.github.com>
2020-11-29 13:21:12 +0100

    Folder and permission checks
    based on best practice, check for /var/lib/lighthouse beacon and validator data folder and permissions
    
    users to check:
    lighthousebeacon
    lighthousevalidator

--- update_lighthouse.sh
+++ update_lighthouse.sh
@@ -4,6 +4,43 @@
 ## Update to new lighthouse version from tags 
 ##############################################
 
+# permission checks
+stat_beacon=$(stat -c "%a %U %G" /var/lib/lighthouse/beacon)
+stat_validators=$(stat -c "%a %U %G" /var/lib/lighthouse/validators)
+stat_git=$(stat -c "%a %U %G" ~/git/lighthouse)
+echo "stat_beacon: $stat_beacon"
+echo "stat_validators: $stat_validators"
+echo "stat_git: $stat_git"
+
+res=0
+if [[ $stat_beacon != *"lighthousebeacon"* ]]; then
+  echo "beacon folder not found or permissions wrong (/var/lib/lighthouse/beacon) - FAIL"
+  let res++
+fi
+if [[ $stat_validators != *"lighthousevalidator"* ]]; then
+  echo "validators folder not found or permissions wrong (/var/lib/lighthouse/validators) - FAIL"
+  let res++
+fi
+if [[ ! -d ~/git/lighthouse ]]; then
+  echo "git folder not found (~/git/lighthouse) - FAIL"
+  let res++
+fi
+
+if [[ $res != 0 ]]; then
+  echo "Errors in data structure found. Check your folders if exists and permissions are set."
+  exit
+fi
+
+while true; do
+    read -p "Start lighthouse update script?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# get last release source from github
 cd ~/git/lighthouse
 git fetch --tags
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
@@ -18,6 +55,7 @@ while true; do
 done
 echo "'git checkout $tag' executed"
 
+# build sources
 while true; do
     read -p "Try to build lighthouse $tag?" yn
     case $yn in
@@ -28,6 +66,7 @@ while true; do
 done
 echo "'make' executed"
 
+# update binary
 while true; do
     read -p "Replace new binary $tag in /usr/local/bin now?" yn
     case $yn in
@@ -38,6 +77,7 @@ while true; do
 done
 echo "binary replaced"
 
+# restart services
 while true; do
     read -p "Restart lighthouse?" yn
     case $yn in
