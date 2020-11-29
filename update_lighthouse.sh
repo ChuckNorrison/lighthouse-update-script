09be171c4978548f7360f8b9e768eb8d9a88c7d4
Chuck Norrison <2964146+ChuckNorrison@users.noreply.github.com>
2020-11-29 09:48:40 +0100

    Create update_lighthouse.sh
    initial version to update a local git repository on ubuntu to the latest release.
    
    path to git is set to ~/git/lighthouse

---
+++ update_lighthouse.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##############################################
+## Update to new lighthouse version from tags 
+##############################################
+
+cd ~/git/lighthouse
+git fetch --tags
+tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+while true; do
+    read -p "Found Release $tag. Checkout?" yn
+    case $yn in
+        [Yy]* ) git checkout $tag; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo "'git checkout $tag' executed"
+
+while true; do
+    read -p "Try to build lighthouse $tag?" yn
+    case $yn in
+        [Yy]* ) make; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo "'make' executed"
+
+while true; do
+    read -p "Restart lighthouse?" yn
+    case $yn in
+        [Yy]* ) sudo systemctl restart lighthouse-beacon.service; sudo systemctl restart lighthouse-validators.service; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo "Job Done"
